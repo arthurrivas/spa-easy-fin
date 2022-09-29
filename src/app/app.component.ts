a3979e8c4a034735390e153b28574f9be4f21527
arthur rivas <arthurrivas1@gmail.com>
2022-09-29 08:46:57 -0300

    criado navegacao entre componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,5 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  options = this._formBuilder.group({
-    bottom: 0,
-    fixed: false,
-    top: 0,
-  });
-
-  constructor(private _formBuilder: FormBuilder) {}
-
-  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(
-    window.location.host
-  );
+  opened = false
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 const routes: Routes = [
   {
-    path: "./dashboard",
+    path: "dashboard",
     component: DashboardComponent
   }
 ]
