0edea0c1182720d12e61b8da184fcb3999e47448
arthur rivas <arthurrivas1@gmail.com>
2022-10-06 08:44:20 -0300

    alterado page profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@ import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+// COMPONENT IMPORT
+import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {
     path: "dashboard",
     component: DashboardComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
   }
 ]
-
 @NgModule({
   imports: [
     BrowserModule,
@@ -28,9 +36,21 @@ const routes: Routes = [
     MatIconModule,
     FormsModule,
     ReactiveFormsModule,
+    MatCardModule,
     RouterModule.forRoot(routes),
   ],
-  declarations: [AppComponent],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule
+  ],
+  declarations: [AppComponent, ProfileComponent, DashboardComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
